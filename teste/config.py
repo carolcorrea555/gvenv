6d6333524654bf4872c3aaec06bad156e0b59479
Carolina Correa <carolcorrea555@gmail.com>
2022-10-31 10:58:24 -0300

    testing .gitignore

---
+++ teste/config.py
@@ -0,0 +1,5 @@
+username = 'cxwxwe'
+password = '775533@cscsenha'
+dsn = 'usbntlricrtdb-scan.io.ad.gmfinancial.com'
+port = 1531
+encoding = 'UTF-8'
---
+++ teste/main.py
@@ -0,0 +1,8 @@
+import cx_Oracle
+import config
+
+sql = 'SELECT *'\
+    'FROM T1_OS_ADMIN.OSUSR_D69_GMFDMREQ req'
+
+print(sql)
+
