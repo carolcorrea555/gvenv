f9cabaeb587ea4314b3b5ce621ef9e1645e25f21
Carolina Correa <carolcorrea555@gmail.com>
2022-10-31 22:43:23 -0300

    adding openpyxl

--- teste/config.py
+++
@@ -1,8 +0,0 @@
-import getpass
-
-username = input('username=')
-password = getpass.getpass('password=')
-host = 'usbntlricrtdb-scan.io.ad.gmfinancial.com'
-service_name = 'CRT2PRDSB'
-port = 1531
-encoding = 'UTF-8'
